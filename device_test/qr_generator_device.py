ab627a2b1997de2b701bfd7c248939b46b25d29e
Van Sang <166635861+Sengpai@users.noreply.github.com>
2026-05-13 14:43:31 +0700

    update port 5000

--- device_test/qr_generator_device.py
+++ device_test/qr_generator_device.py
@@ -2,7 +2,7 @@ import requests
 import qrcode
 from PIL import Image
 
-BASE_URL = "http://127.0.0.1:3000/api"  # Chỉnh lại theo be
+BASE_URL = "http://127.0.0.1:5000/api"  # Chỉnh lại theo be
 
 def generate_qr():
     r = requests.post(f"{BASE_URL}/qr/generate")
